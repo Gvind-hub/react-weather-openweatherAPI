5c2daee1bfbd4895c90be0700f59a66dac17292a
Gvind-hub <63408287+Gvind-hub@users.noreply.github.com>
2020-06-03 20:50:00 +0300

    Updates

---
+++ precache-manifest.8edb6db2c4eb6eb1ea5f14dbb1e75c8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d18d28f2b64bbc08155b04ea9d34bd",
+    "url": "/react-weather-openweatherAPI/index.html"
+  },
+  {
+    "revision": "c93c9c42c91b99fdc785",
+    "url": "/react-weather-openweatherAPI/static/css/main.410098b0.chunk.css"
+  },
+  {
+    "revision": "4e2a63cd240c1adb0ae6",
+    "url": "/react-weather-openweatherAPI/static/js/2.903fbdc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-openweatherAPI/static/js/2.903fbdc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93c9c42c91b99fdc785",
+    "url": "/react-weather-openweatherAPI/static/js/main.64495fca.chunk.js"
+  },
+  {
+    "revision": "6c0e1898aa59ae7073c9",
+    "url": "/react-weather-openweatherAPI/static/js/runtime-main.4fcb46f8.js"
+  }
+]);
